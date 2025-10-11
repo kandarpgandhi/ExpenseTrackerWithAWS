6938fe46383132fd7fc7cabb80923050b4ba075b
kandarpgandhi <kgandhi2206@gmail.com>
2025-10-11 13:02:34 +0530

    Updated index.html,userController.js and added a new file login.html

--- controller/userController.js
+++ controller/userController.js
@@ -4,6 +4,12 @@ const sequelize = require('../utils/db-connection')
 const addUser = async (req, res) => {
     try {
         const { userName, email, password } = req.body
+
+        const existingUser = await User.findOne({ where: { email } })
+
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists with this email' })
+        }
         const user = await User.create({
             userName: userName,
             email: email,
@@ -16,5 +22,27 @@ const addUser = async (req, res) => {
 }
 
 
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
 
-module.exports = { addUser }
+    try {
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        if (password != user.password) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        res.status(200).json({ message: 'Login successful' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+
+
+module.exports = { addUser, loginUser }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const userController = require('../controller/userController')
 
 router.post('/', userController.addUser)
+router.post('/login', userController.loginUser);
 // router.get('/', userController.getProduct)
 // router.put('/:id', userController.updateProduct)
 
